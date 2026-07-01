42a1d4086d834fb750ac02738c4f5fa540010556
Irecharge-sof <sofiamarley29@gmail.com>
2026-07-01 17:02:40 +0200

    debug layout step 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
-import { useState } from 'react'
 import Navbar from './composants/Navbar'
-import { Routes, Route } from 'react-router-dom'
-import  Acceuil  from './pages/Acceuil'
-import  Activation  from './pages/Activation'
-import  Remboursement  from './pages/Remboursement'
 import Footers from './composants/Footers'
-
-
-
-import './App.css'
 
 export default function App() {
   return (
-    <div style={{
-      color: "white",
-      fontSize: "30px",
-      padding: "50px"
-    }}>
-      APP OK 🚀
-    </div>
+    <>
+      <Navbar />
+      <div style={{color:"white", padding:"20px"}}>
+        MIDDLE TEST 🚀
+      </div>
+      <Footers />
+    </>
   )
 }

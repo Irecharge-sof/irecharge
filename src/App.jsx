044254e8667a684e3bc2ba25563d1d79c8b0d01c
Irecharge-sof <sofiamarley29@gmail.com>
2026-07-01 17:41:30 +0200

    fix css assets background

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import Navbar from './composants/Navbar'
 import Footers from './composants/Footers'
+import bg from './assets/arriereplan.jpeg'
 
 export default function App() {
   return (
-    <>
+      <section style={{
+          backgroundImage: url(${bg}),
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+             minHeight: "100vh"
+            }}>
       <Navbar />
       <div style={{color:"white", padding:"20px"}}>
         MIDDLE TEST 🚀
       </div>
       <Footers />
-    </>
+    </section>
+    
   )
 }
